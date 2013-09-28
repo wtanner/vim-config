54a2e4d2f529a6fff80266abf6307cb51d356e23
Wesley Tanner <wesley.tanner@gmail.com>
2013-09-27 19:03:16 -0700

    inital import

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+mkdir ~/.vim/_backup
+mkdir ~/.vim/_temp
+ln -s ~/.vim/.vimrc ~/.vimrc
+vim +BundleInstall +qall
